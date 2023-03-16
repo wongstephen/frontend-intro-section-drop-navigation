0ed66656cbf7a9e9faf4110874f98e969f509c51
Stephen Wong <wongstephenky@gmail.com>
2023-03-15 17:58:48 -0700

    final commit

---
+++ main.js
@@ -0,0 +1,39 @@
+const hamburgerBtn = document.querySelector(".menu-icon");
+const closeMenuBtn = document.querySelector(".close-menu-icon");
+const mobileNavContainer = document.querySelector(".mobile-nav-container");
+const arrows = document.querySelectorAll(".arrow");
+const expandLinkBtns = document.querySelectorAll(".nav-link-button");
+const featuresSubMenu = document.querySelectorAll(".sub-list");
+const links = document.querySelectorAll(".nav-link");
+
+// open and close btn
+function toggleNav() {
+  mobileNavContainer.classList.toggle("opacity0");
+}
+hamburgerBtn.addEventListener("click", toggleNav);
+closeMenuBtn.addEventListener("click", toggleNav);
+
+// expand menu btn
+function handleExpandNav(expandLink) {
+  expandLink.childNodes[1].classList.toggle("flipped-arrow");
+  if (expandLink.name === "feature") {
+    featuresSubMenu[0].classList.toggle("hide-sub-list");
+    featuresSubMenu[1].classList.add("hide-sub-list");
+  }
+  if (expandLink.name === "company") {
+    featuresSubMenu[1].classList.toggle("hide-sub-list");
+    featuresSubMenu[0].classList.add("hide-sub-list");
+  }
+}
+expandLinkBtns.forEach((el) =>
+  el.addEventListener("click", () => {
+    handleExpandNav(el);
+  })
+);
+
+// close menu
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    toggleNav();
+  });
+});
